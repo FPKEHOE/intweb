5d48e5350d285084407ef4f426af071b63d135cd
Franny <fpkehoe@dons.usfca>
2025-05-05 06:51:10 -0700

    sdg

--- sketch2/sketch.js
+++ sketch2/sketch.js
@@ -1,41 +1,39 @@
 var gif;
+var bckgrnd;
+var c = 0;
 
 
 function preload(){
-  gif=loadImage("assets/vinefinal.gif");
-
-  
-  goudy=loadFont("assets/goudy.ttf");
-  spirax=loadFont("assets/spirax.ttf");
-  
-
+  gif=loadImage("assets/omg.gif");  
 
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  createCanvas(1150, 735);
+
 }
 
 function draw() {  
   background('#182D09');
-  imageMode(CORNER);
-  image(gif, 0, 0);
+  imageMode(CENTER);
+  if(mouseIsPressed){
+    if(c<153){
+    c++;}
+    //this re-sets it if you go past the last frame in your gif
+    if (c > 153){
+      c = 0;
+    }
+  }
+  image(gif,width/2,height/2,1044,735);
+  gif.setFrame(c);
+  gif.setFrame(c);
+  gif.setFrame(c);
+  gif.setFrame(c);
+  gif.setFrame(c);
+  gif.setFrame(c);
+  gif.setFrame(c);
 
 }
   
-function mousePressed() {
-  gif.play();
-}
-
-// Play the GIF when the user releases the mouse.
-function mouseReleased() {
-  gif.pause();
-}
 
 
-
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-  
-}
